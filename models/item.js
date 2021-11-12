b688fb2ece4a21473717f98b31ed497f3d0aeb01
Gred <jihong.park.dev@gmail.com>
2021-11-12 16:19:36 +0900

    swagger 문서 추가

--- models/item.js
+++ models/item.js
@@ -1,47 +1,47 @@
 module.exports = (sequelize, DataTypes) => {
-    const Item = sequelize.define(
-        'Item',
-        {
-            item_id: {
-                type: DataTypes.STRING(),
-                allowNull: false,
-                unique: true,
-            },
-            item_name: {
-                type: DataTypes.STRING(),
-                allowNull: false,
-            },
-            brand_name: {
-                type: DataTypes.STRING(),
-            },
-            item_link: {
-                type: DataTypes.STRING(),
-            },
+	const Item = sequelize.define(
+		'Item',
+		{
+			item_id: {
+				type: DataTypes.STRING(),
+				allowNull: false,
+				unique: true,
+			},
+			item_name: {
+				type: DataTypes.STRING(),
+				allowNull: false,
+			},
+			brand_name: {
+				type: DataTypes.STRING(),
+			},
+			item_link: {
+				type: DataTypes.STRING(),
+			},
 
-            // 원래금액
-            strike_out_price: {
-                type: DataTypes.STRING(),
-            },
-            // 노출금액
-            display_price: {
-                type: DataTypes.STRING(),
-            },
+			// 원래금액
+			strike_out_price: {
+				type: DataTypes.STRING(),
+			},
+			// 노출금액
+			display_price: {
+				type: DataTypes.STRING(),
+			},
 
-            is_adult_item: {
-                type: DataTypes.BOOLEAN,
-            },
-        },
-        {
-            charset: 'utf8',
-            colate: 'utf8_general_ci',
-        }
-    );
+			is_adult_item: {
+				type: DataTypes.BOOLEAN,
+			},
+		},
+		{
+			charset: 'utf8',
+			colate: 'utf8_general_ci',
+		}
+	);
 
-    Item.associate = (db) => {
-        db.Item.hasMany(db.ItemImage);
-        db.Item.belongsToMany(db.User, { through: 'Clip', as: 'ClipUser', foreignKey: 'item_id' });
-        db.Item.belongsTo(db.ModuleStore, { foreignKey: 'module_store_id' });
-    };
+	Item.associate = (db) => {
+		db.Item.hasMany(db.ItemImage);
+		db.Item.belongsToMany(db.User, { through: 'Clip', as: 'ClipUser', foreignKey: 'item_id' });
+		db.Item.belongsTo(db.ModuleStore, { foreignKey: 'module_store_id' });
+	};
 
-    return Item;
+	return Item;
 };
--- routes/item.js
+++ routes/item.js
@@ -1,74 +1,77 @@
 const express = require('express');
 
 const { Item, ItemImage } = require('../models');
+
 const router = express.Router();
 
 router.get('/:item_id', async (req, res, next) => {
-    console.log('item');
-    console.log(req.params);
-    try {
-        const item = await Item.findOne({
-            where: { item_id: req.params.item_id },
-        });
+	console.log('item');
+	console.log(req.params);
+	try {
+		const item = await Item.findOne({
+			where: { item_id: req.params.item_id },
+		});
 
-        if (!item) {
-            return res.status(404).status('cannot get item');
-        }
+		if (!item) {
+			return res.status(404).status('cannot get item');
+		}
 
-        const imageItem = await Item.findOne({
-            where: { item_id: item.item_id },
-            include: [{ model: ItemImage }],
-        });
+		const imageItem = await Item.findOne({
+			where: { item_id: item.item_id },
+			include: [{ model: ItemImage }],
+		});
 
-        const result = {
-            res_code: '200',
-            res_status: 'success',
-            item: imageItem,
-        };
+		const result = {
+			res_code: '200',
+			res_status: 'success',
+			data: imageItem,
+		};
 
-        res.status(200).json(result);
-    } catch (error) {
-        console.log(error);
-        next(error);
-    }
+		res.status(200).json(result);
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
 });
 
-router.patch('/cliped', async (req, res) => {
-    console.log('cliped');
-    console.log(req.query);
-    try {
-        const { item_id, user_id } = req.query;
-        const item = await Item.findOne({
-            where: { item_id },
-        });
-        if (!item) {
-            return res.status(404).status('cannot get item');
-        }
-        await item.addClipUser(user_id);
-        return res.status(200).json({ res_code: '200', res_status: 'cliped success', item_id, user_id });
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
+router.patch('/clipped', async (req, res) => {
+	console.log('clipped');
+	console.log(req.query);
+	try {
+		const { item_id, user_id } = req.query;
+		const item = await Item.findOne({
+			where: { item_id },
+		});
+		if (!item) {
+			return res.status(404).status('cannot get item');
+		}
+		await item.addClipUser(user_id);
+		return res.status(200).json({ res_code: '200', res_status: 'clipped success', item_id, user_id });
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
 });
 
-router.delete('/uncliped', async (req, res) => {
-    console.log('uncliped');
-    console.log(req);
-    try {
-        const { item_id, user_id } = req.query;
-        const item = await Item.findOne({
-            where: { item_id },
-        });
-        if (!item) {
-            return res.status(404).status('cannot get item');
-        }
-        await item.removeClipUser(user_id);
-        return res.status(200).json({ res_code: '200', res_status: 'cliped success', item_id, user_id });
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
+router.delete('/unclipped', async (req, res) => {
+	console.log('unclipped');
+	console.log(req);
+	try {
+		const { item_id, user_id } = req.query;
+		const item = await Item.findOne({
+			where: { item_id },
+		});
+
+		console.log(item);
+		if (!item) {
+			return res.status(404).status('cannot get item');
+		}
+		await item.removeClipUser(user_id);
+		return res.status(200).json({ res_code: '200', res_status: 'clipped delete success', item_id, user_id });
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
 });
 
 module.exports = router;
